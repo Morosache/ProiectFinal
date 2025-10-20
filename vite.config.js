bd7bdd51e39c03536b4664ec885478d612626702
Elisei Morosan <elisei.morosan@emanuel.ro>
2025-10-20 13:21:33 +0300

    made adjustments

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/ProiectFinal/',
+  base:'',
   plugins: [
     vue(),
     vueDevTools(),
